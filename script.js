00ddae68423e20ffa38d9cc8ecf5f28d41fe7f1f
noahthecodeninja <noahcapucillishata@uagateway.org>
2016-11-28 21:15:46 +0000

    I'm beautifuul

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
+    $("button").click(function() {
+        $("#like").html("You like this project!");
     });
     
-    
+    $("#profile").click(function() {
+        $("#beautiful").html("I sure am beautiful, aren't I?");
+    });
     
     
 });
